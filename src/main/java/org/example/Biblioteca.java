54c0ce0008a313f421005a93e2faedde8e11b4f8
Katherine Robayo <akatherinerobayo@ucundinamarca.edu.co>
2025-03-04 18:41:39 -0500

    Documentación del código en la clase biblioteca y parte del main

--- src/main/java/org/example/Biblioteca.java
+++ src/main/java/org/example/Biblioteca.java
@@ -1,50 +1,51 @@
-package org.example;
+package org.example;// Define el paquete donde se encuentra la clase Biblioteca
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList;// Importa la clase ArrayList para manejar listas  de libros
+import java.util.List; // Importa la interfaz List para trabajar con listas de libros
 
-public class Biblioteca {
-        private List<Libro> libros;
+public class Biblioteca { // se define la clase Biblioteca
+        private List<Libro> libros; //Lista privada que almacena los libros en la biblioteca
 
-        // Metodo Constructor
+        // Metodo Constructor de la clase biblioteca
         public Biblioteca() {
-            this.libros = new ArrayList<>();
+            this.libros = new ArrayList<>(); //Inicializa la lista de libros como un ArrayList vacío
         }
 
-        // Agregar un libro
+        //  metodo para agregar un libro
         public void agregarLibro(Libro libro) {
-            libros.add(libro);
+            libros.add(libro); // Agrega el libro recibido como parámetro a la lista de libros
         }
 
-        // Buscar libros por título
+        // metodo para buscar libros por título
         public List<Libro> buscarPorTitulo(String titulo) {
-            List<Libro> resultado = new ArrayList<>();
-            for (Libro libro : libros) {
-                if (libro.getTitulo().toLowerCase().contains(titulo.toLowerCase())) {
-                    resultado.add(libro);
+            List<Libro> resultado = new ArrayList<>();// Crea una lista para almacenar los libros que coincidan con el título
+            for (Libro libro : libros) {// Recorre la lista de libros
+                if (libro.getTitulo().toLowerCase().contains(titulo.toLowerCase())) { // Compara el título ingresado con los títulos de los libros (ignorando mayúsculas/minúsculas)
+                    resultado.add(libro); // Si hay coincidencia, se agrega el libro a la lista de resultados
                 }
             }
-            return resultado;
+            return resultado;// Muestra la lista de libros que coinciden con el título
         }
 
-        //  Buscar libros por autor
+
+        //  Metodo para buscar libros por autor
         public List<Libro> buscarPorAutor(String autor) {
-            List<Libro> resultado = new ArrayList<>();
-            for (Libro libro : libros) {
-                if (libro.getAutor().toLowerCase().contains(autor.toLowerCase())) {
+            List<Libro> resultado = new ArrayList<>();// Crea una lista para almacenar los libros que coincidan con el autor
+            for (Libro libro : libros) { // Recorre la lista de libros
+                if (libro.getAutor().toLowerCase().contains(autor.toLowerCase())) {// Compara el autor ingresado con los autores de los libros (ignorando mayúsculas/minúsculas)
                     resultado.add(libro);
                 }
             }
-            return resultado;
+            return resultado;// Muestra la lista de libros que coinciden con el título
         }
 
-        // listar todos los libros
+        // Metodo para listar todos los libros
         public void listarLibros() {
-            if (libros.isEmpty()) {
-                System.out.println("No hay libros disponibles en este momento.");
-            } else {
-                for (Libro libro : libros) {
-                    libro.mostrarInfo();
+            if (libros.isEmpty()) { // Verifica si la lista de libros está vacía
+                System.out.println("No hay libros disponibles en este momento.");// muestra ensaje al usuario de si no hay libros
+            } else {// Si hay libros en la biblioteca
+                for (Libro libro : libros) { // Recorre la lista de libros
+                    libro.mostrarInfo(); //muestra la información de cada libro
                 }
             }
         }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
-package org.example;
+package org.example; // define el paquete de donde se  encuentra la clase main
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.List; // Importa la interfaz List para trabajar con listas de libros
+import java.util.Scanner; // importamos la clase Scanner para recibir la entrada del usuario
 
-public class Main {
+public class Main { // se define la clase main
     public static void main(String[] args) {
 
         Scanner entrada=new Scanner(System.in);
@@ -38,25 +38,24 @@ public class Main {
                     System.out.println("Libro agregado exitosamente");
                     break;
 
-                case 2:
-                    //Buscar libros por titulos
-                    System.out.println("Ingrese el titulo que desa buscar");
-                    String tituloBuscar = entrada.nextLine();
-                    List <Libro> librosPorTitulo = biblioteca.buscarPorTitulo(tituloBuscar);
-                    if (librosPorTitulo.isEmpty()) {
-                        System.out.println("No se encontraron libros con ese titulo");
-                    } else {
-                        for (Libro libro: librosPorTitulo) {
-                            libro.mostrarInfo();
+                case 2: // Opción 2 buscar libros por titulos
+                    System.out.println("Ingrese el titulo que desa buscar");// se pide al usuario ingresar el titulo del libro que desea buscar
+                    String tituloBuscar = entrada.nextLine(); // recibe el titulo que ingreso el usuario
+                    List <Libro> librosPorTitulo = biblioteca.buscarPorTitulo(tituloBuscar); //Busca  en la lista los libros con ese  título
+                    if (librosPorTitulo.isEmpty()) { // Verifica si no se encontraron libros
+                        System.out.println("No se encontraron libros con ese titulo"); //muestra un mensaje al usuario confirmando que no hay coincidencias
+                    } else { //si se encuentran libros con ese nombre, se muestran
+                        for (Libro libro: librosPorTitulo) { //recorre la lista de todos los libros que se encontraron
+                            libro.mostrarInfo(); //muestra la información de cada libro
                         }
                     }
-                    break;
+                    break; // cierra  la ejecución del caso 2 y vuelve al menú
 
-                case 3:
-                    System.out.println("Ingrese el autor que desea buscar");
-                    String autorBuscar = entrada.nextLine();
-                    List<Libro> librosPorAutor = biblioteca.buscarPorAutor(autorBuscar);
-                    if (librosPorAutor.isEmpty()) {
+                case 3: //Opción 3 buscar libros por autor
+                    System.out.println("Ingrese el  nombre del autor que desea buscar"); // se pide al usuario ingresar el nombre del autor
+                    String autorBuscar = entrada.nextLine();// recibe el nombre que ingreso el usuario
+                    List<Libro> librosPorAutor = biblioteca.buscarPorAutor(autorBuscar); // Busca en la lista los libros con ese autor
+                    if (librosPorAutor.isEmpty()) { // verifica si  no hay alguna coincidencia
                         System.out.println("No se encontraron libros de ese autor");
 
                     } else {
@@ -64,20 +63,20 @@ public class Main {
                             libro.mostrarInfo();
                         }
                     }
+                    break; //cierra la ejecución del caso 3 y vuelve al menú
+
+                case 4: // opción 4 enlistar libros
+                    biblioteca.listarLibros();// crea la lista de los libros agregados en la biblioteca
                     break;
 
-                case 4:
-                    biblioteca.listarLibros();
-                    break;
+                case 5 : //Opción 5 salir
+                    System.out.println("¡Gracias por usar nuestra biblioteca!"); //Muestra al usuario un mensaje de despedida
+                    return; //termina la ejecución del programa
 
-                case 5 :
-                    System.out.println("¡Gracias por usar nuestra biblioteca!");
-                    return;
+                default: //Opción inválida (cuando el usuario ingresa un número diferente entre 1 y 5)
+                    System.out.println("Opción invalida, por favor intentelo nuevamente"); //Muestra un mensaje al usuario de error
 
-                default:
-                    System.out.println("Opción invalida, por favor intentelo nuevamente");
-
-                    break;
+                    break;// Se rompe la ejecucion del caso default  y vuelve al menú
 
 
             }
